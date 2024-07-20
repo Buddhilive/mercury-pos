61896a3aab593484075971d62d0836bd61f522ad
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-20 21:43:44 +0530

    Add login endpoint

--- api/backend/src/controllers/userRouter.ts
+++ api/backend/src/controllers/userRouter.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { db_connection } from '../db_connection';
 import { QueryError, QueryResult } from 'mysql2';
+import * as jwt from 'jsonwebtoken';
 
 const userRouter = express.Router();
 
@@ -12,7 +13,7 @@ userRouter.post('/signup', (req, res) => {
             if ((results as Array<QueryResult>).length <= 0) {
                 query = 'insert into mp_users (name,contactNumber,email,password,status,role) values(?,?,?,?,"false","user")';
                 return db_connection.query(query, [user['name'], user['contactNumber'], user['email'], user['password']], (err: QueryError) => {
-                    if(!err) {
+                    if (!err) {
                         return res.status(200).json({
                             message: "User registered successfully"
                         });
@@ -29,6 +30,41 @@ userRouter.post('/signup', (req, res) => {
         } else {
             return res.status(500).json({
                 message: 'Error in signup',
+                error: err
+            });
+        }
+    });
+});
+
+userRouter.post('/login', (req, res) => {
+    const user = req.body;
+    const query = "SELECT email, password, role, status from mp_users where email=?";
+    db_connection.query(query, user['email'], (err: QueryError, results: QueryResult) => {
+        if (!err) {
+            if ((results as Array<QueryResult>).length <= 0 || results[0]['password'] != user['password']) {
+                return res.status(401).json({
+                    message: 'Incorrect username or password'
+                });
+            } else if (results[0]['status'] === 'false') {
+                return res.status(401).json({
+                    message: 'Account not approved by Admin'
+                });
+            } else if (results[0]['password'] === user['password']) {
+                const response = { email: results[0]['email'], role: results[0]['role'] };
+                const accessToken = jwt.sign(response, process.env['ACCESS_TOKEN'], { expiresIn: '8h' });
+                return res.status(200).json({
+                    message: 'Login success',
+                    token: accessToken
+                });
+            } else {
+                return res.status(400).json({
+                    message: 'Login unsuccessful. Please try again later.',
+                    error: err
+                });
+            }
+        } else {
+            return res.status(500).json({
+                message: 'Login error',
                 error: err
             });
         }
