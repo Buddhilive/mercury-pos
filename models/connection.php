c534306e23d7b0d97435cb465f8eac63e51b718e
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-21 23:08:52 +0530

    Add database connection

---
+++ models/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+class Connection {
+    public function connect(){
+		$link = new PDO("mysql:host=localhost;dbname=mercury_pos", "root", "");
+		$link -> exec("set names utf8");
+
+		return $link;
+	}
+}
