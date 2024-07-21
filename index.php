68ea83e9a896c801dd96daf52dfdd0e8110770fd
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-21 21:28:32 +0530

    Import all controllers

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
     require_once "controllers/template.controller.php";
+    require_once "controllers/users.controller.php";
+    require_once "controllers/categories.controller.php";
+    require_once "controllers/products.controller.php";
+    require_once "controllers/customers.controller.php";
+    require_once "controllers/sales.controller.php";
 
     $template = new TemplateController();
     $template -> ctrTemplate()
