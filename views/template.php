1a5c0882b6379d287918afd63be88607f039f04a
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-21 19:46:36 +0530

    Add footer module

--- views/template.php
+++ views/template.php
@@ -79,12 +79,10 @@
         </div>
         <!-- /.content-wrapper -->
 
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 0.0.1
-            </div>
-            <strong>Copyright &copy; 2024 <a href="https://buddhilive.com">Buddhilive</a>.</strong> All rights reserved.
-        </footer>
+        <!-- Footer -->
+        <?php
+        include "modules/footer.php";
+        ?>
 
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
