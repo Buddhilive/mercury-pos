e7dfc02ead18c80e30ef7209ac989b64409c6632
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-21 20:07:15 +0530

    update footer

--- views/modules/footer.php
+++ views/modules/footer.php
@@ -1,6 +1,10 @@
+<?php
+$currentYear = date("Y");
+?>
+
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 0.0.1
     </div>
-    <strong>Copyright &copy; 2024 <a href="https://buddhilive.com">Buddhilive</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; <?php echo $currentYear; ?> <a href="https://buddhilive.com">Buddhilive</a>.</strong> All rights reserved.
 </footer>
