52f958841cb4c9470703845a96fdaa343eca076f
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-21 16:41:34 +0530

    Reset Project

--- api/backend/src/services/checkRoles.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export function checkRole(req: Request, res: Response, next: NextFunction) {
-    if(res.locals['role'] == 'user') {
-        res.sendStatus(401);
-    } else {
-        next();
-    }
-}
