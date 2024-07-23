70a766ea5d6d9d2dc0fd1161b963b80d7735f8b2
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-23 23:35:24 +0530

    add update user function to users ajax

--- ajax/users.ajax.php
+++ ajax/users.ajax.php
@@ -14,6 +14,31 @@ class AjaxUsers{
 
 		echo json_encode($answer);
 	}
+
+	/* ACTIVATE USER */
+	public $activateUser;
+	public $activateId;	
+
+	public function ajaxActivateUser(){
+		$table = "mp_users";
+		$item1 = "status";
+		$value1 = $this->activateUser;
+
+		$item2 = "id";
+		$value2 = $this->activateId;
+
+		$answer = UsersModel::updateUser($table, $item1, $value1, $item2, $value2);
+	}
+
+	/* VALIDATE IF USER ALREADY EXISTS */
+	public $validateUser;
+
+	public function ajaxValidateUser(){
+		$item = "username";
+		$value = $this->validateUser;
+		$answer = UserController::getAllUsers($item, $value);
+		echo json_encode($answer);
+	}
 }
 
 /* Edit user */
@@ -21,4 +46,19 @@ if (isset($_POST["idUser"])) {
 	$edit = new AjaxUsers();
 	$edit -> idUser = $_POST["idUser"];
 	$edit -> ajaxEditUser();
+}
+
+/* Activate user */
+if (isset($_POST["activateUser"])) {
+	$activateUser = new AjaxUsers();
+	$activateUser -> activateUser = $_POST["activateUser"];
+	$activateUser -> activateId = $_POST["activateId"];
+	$activateUser -> ajaxActivateUser();
+}
+
+/* Validate User */
+if (isset($_POST["validateUser"])) {
+	$valUser = new AjaxUsers();
+	$valUser -> validateUser = $_POST["validateUser"];
+	$valUser -> ajaxValidateUser();
 }
