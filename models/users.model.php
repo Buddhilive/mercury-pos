75e797381328d95bf17ef095ccf8de624e4115f6
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-23 23:35:45 +0530

    add Update user method to users model

--- models/users.model.php
+++ models/users.model.php
@@ -55,4 +55,19 @@ class UsersModel
 
 		$stmt = null;
 	}
+
+	static public function updateUser($table, $item1, $value1, $item2, $value2){
+		$stmt = Connection::connect()->prepare("UPDATE $table set $item1 = :$item1 WHERE $item2 = :$item2");
+
+		$stmt -> bindParam(":".$item1, $value1, PDO::PARAM_STR);
+		$stmt -> bindParam(":".$item2, $value2, PDO::PARAM_STR);
+
+		if ($stmt->execute()) {			
+			return 'ok';		
+		} else {
+			return 'error';		
+		}
+
+		$stmt = null;
+	}
 }
