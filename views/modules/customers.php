fb17d26e85ad60f78a83ecc0fb58f2badcf552e5
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-27 20:51:05 +0530

    Design customers page

--- views/modules/customers.php
+++ views/modules/customers.php
@@ -8,7 +8,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
                         <li class="breadcrumb-item active">Clients</li>
                     </ol>
                 </div>
@@ -18,32 +18,231 @@
 
     <!-- Main content -->
     <section class="content">
-
-        <!-- Default box -->
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Title</h3>
-
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                        <i class="fas fa-times"></i>
-                    </button>
-                </div>
+                <button class="btn btn-primary" data-toggle="modal" data-target="#addCustomer">
+                    Add User
+                </button>
             </div>
             <div class="card-body">
-                Start creating your amazing application!
-            </div>
-            <!-- /.card-body -->
-            <div class="card-footer">
-                Footer
-            </div>
-            <!-- /.card-footer-->
-        </div>
-        <!-- /.card -->
+                <table class="table table-bordered table-striped table-responsive-lg customerTable">
+                    <thead>
+                        <tr>
+                            <th style="width:10px">#</th>
+                            <th>Name</th>
+                            <th>I.D Document</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                            <th>Birthday</th>
+                            <th>Total purchases</th>
+                            <th>Last Purchase</th>
+                            <th>Last login</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
 
+                        $item = null;
+                        $value = null;
+
+                        $Customers = CustomersController::showCustomers($item, $value);
+
+                        foreach ($Customers as $key => $customer) {
+                            echo '<tr>
+                                    <td>' . ($key + 1) . '</td>
+                                    <td>' . $customer["name"] . '</td>
+                                    <td>' . $customer["idDocument"] . '</td>
+                                    <td>' . $customer["email"] . '</td>
+                                    <td>' . $customer["phone"] . '</td>
+                                    <td>' . $customer["address"] . '</td>
+                                    <td>' . $customer["birthdate"] . '</td>             
+                                    <td>' . $customer["purchases"] . '</td>
+                                    <td>0000-00-00 00:00:00</td>
+                                    <td>' . $customer["registerDate"] . '</td>
+                                    <td>
+                                        <div class="btn-group">      
+                                        <button class="btn btn-warning btnEditCustomer" data-toggle="modal" data-target="#editCustomerModal" idCustomer="' . $customer["id"] . '"><i class="fa fa-pencil-alt"></i></button>
+                                        <button class="btn btn-danger btnDeleteCustomer" idCustomer="' . $customer["id"] . '"><i class="fa fa-times"></i></button>
+                                        </div>  
+                                    </td>
+                                </tr>';
+                        }
+
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </section>
     <!-- /.content -->
 </div>
+
+<!-- Add new customer modal -->
+<div id="addCustomer" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Add customer</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form role="form" method="POST" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input class="form-control input-lg" type="text" name="newCustomer" placeholder="Write name" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input class="form-control input-lg" type="number" min="0" name="newIdDocument" placeholder="Write your ID" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input class="form-control input-lg" type="text" name="newEmail" placeholder="Email" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input class="form-control input-lg" type="text" name="newPhone" placeholder="phone" data-inputmask='"mask": "(999) 999-9999999"' data-mask required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input class="form-control input-lg" type="text" name="newAddress" placeholder="Address" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input class="form-control input-lg" type="text" name="newBirthdate" placeholder="Birth Date" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+                <?php
+                $createCustomer = new CustomersController();
+                $createCustomer->createCustomer();
+                ?>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Edit customer modal -->
+<div id="editCustomerModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Edit customer</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form role="form" method="POST" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input type="text" class="form-control input-lg" name="editCustomer" id="editCustomer" required>
+                                <input type="hidden" id="idCustomer" name="idCustomer">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input type="number" min="0" class="form-control input-lg" name="editIdDocument" id="editIdDocument" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input type="email" class="form-control input-lg" name="editEmail" id="editEmail" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input type="text" class="form-control input-lg" name="editPhone" id="editPhone" data-inputmask="'mask':'(999) 999-9999999'" data-mask required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input type="text" class="form-control input-lg" name="editAddress" id="editAddress"  required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-tag"></i></span>
+                                </div>
+                                <input type="text" class="form-control input-lg" name="editBirthdate" id="editBirthdate" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+                <?php
+                $EditCustomer = new CustomersController();
+                $EditCustomer -> editCustomer();
+                ?>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php
+  $deleteCustomer = new CustomersController();
+  $deleteCustomer -> deleteCustomer();
+?>
