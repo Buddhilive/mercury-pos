233b485e49316381e5cf51a7bf865a95e59098a8
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-28 21:36:42 +0530

    Add sales model

---
+++ models/sales.model.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once 'connection.php';
+
+class SalesModel
+{
+    static public function showSales($table, $item, $value)
+    {
+        if ($item != null) {
+            $stmt = Connection::connect()->prepare("SELECT * FROM $table WHERE $item = :$item ORDER BY id ASC");
+            $stmt->bindParam(":" . $item, $value, PDO::PARAM_STR);
+            $stmt->execute();
+
+            return $stmt->fetch();
+        } else {
+            $stmt = Connection::connect()->prepare("SELECT * FROM $table ORDER BY id ASC");
+            $stmt->execute();
+
+            return $stmt->fetchAll();
+        }
+
+        $stmt = null;
+    }
+
+    static public function addSale($table, $data)
+    {
+        $stmt = Connection::connect()->prepare("INSERT INTO $table(code, idCustomer, idSeller, products, tax, netPrice, totalPrice, paymentMethod) VALUES (:code, :idCustomer, :idSeller, :products, :tax, :netPrice, :totalPrice, :paymentMethod)");
+
+        $stmt->bindParam(":code", $data["code"], PDO::PARAM_INT);
+        $stmt->bindParam(":idCustomer", $data["idCustomer"], PDO::PARAM_INT);
+        $stmt->bindParam(":idSeller", $data["idSeller"], PDO::PARAM_INT);
+        $stmt->bindParam(":products", $data["products"], PDO::PARAM_STR);
+        $stmt->bindParam(":tax", $data["tax"], PDO::PARAM_STR);
+        $stmt->bindParam(":netPrice", $data["netPrice"], PDO::PARAM_STR);
+        $stmt->bindParam(":totalPrice", $data["totalPrice"], PDO::PARAM_STR);
+        $stmt->bindParam(":paymentMethod", $data["paymentMethod"], PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+
+        $stmt = null;
+    }
+
+    static public function editSale($table, $data)
+    {
+        $stmt = Connection::connect()->prepare("UPDATE $table SET  idCustomer = :idCustomer, idSeller = :idSeller, products = :products, tax = :tax, netPrice = :netPrice, totalPrice= :totalPrice, paymentMethod = :paymentMethod WHERE code = :code");
+
+        $stmt->bindParam(":code", $data["code"], PDO::PARAM_INT);
+        $stmt->bindParam(":idCustomer", $data["idCustomer"], PDO::PARAM_INT);
+        $stmt->bindParam(":idSeller", $data["idSeller"], PDO::PARAM_INT);
+        $stmt->bindParam(":products", $data["products"], PDO::PARAM_STR);
+        $stmt->bindParam(":tax", $data["tax"], PDO::PARAM_STR);
+        $stmt->bindParam(":netPrice", $data["netPrice"], PDO::PARAM_STR);
+        $stmt->bindParam(":totalPrice", $data["totalPrice"], PDO::PARAM_STR);
+        $stmt->bindParam(":paymentMethod", $data["paymentMethod"], PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+
+        $stmt = null;
+    }
+
+    static public function deleteSale($table, $data)
+    {
+        $stmt = Connection::connect()->prepare("DELETE FROM $table WHERE id = :id");
+        $stmt->bindParam(":id", $data, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+
+        $stmt = null;
+    }
+}
