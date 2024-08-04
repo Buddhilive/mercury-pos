8580817568909b2c0e36ccbffda56392862ac12b
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-04 23:41:27 +0530

    Update logo

--- views/modules/sidebar.php
+++ views/modules/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="views/index3.html" class="brand-link">
-        <img src="views/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="views/assets/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Mercury POS</span>
     </a>
 
