66856b0414ae9237ad9dc8439f3473a757fecda9
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-05 22:22:43 +0530

    Update SQL script

--- resources/mercury_pos.sql
+++ resources/mercury_pos.sql
@@ -1,15 +1,15 @@
 CREATE TABLE
     `mp_users` (
-        `id` INT (11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- Unique identifier
-        `name` VARCHAR(255) NOT NULL, -- Name with appropriate character limit
-        `username` VARCHAR(50) UNIQUE NOT NULL, -- Username for login (unique)
-        `password` VARCHAR(255) NOT NULL, -- Password (hashed securely)
-        `profile` TEXT, -- Profile details (consider JSON or dedicated table)
-        `photo` VARCHAR(255), -- Photo URL/path (optional)
-        `status` TINYINT (1) NOT NULL DEFAULT '0', -- User status (e.g., active = 1, inactive = 0)
-        `last_login` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Last login timestamp
-        `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- Creation timestamp with auto-updating on update
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8_general_ci;
+        `id` INT (11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(255) NOT NULL,
+        `username` VARCHAR(50) UNIQUE NOT NULL,
+        `password` VARCHAR(255) NOT NULL,
+        `profile` TEXT,
+        `photo` VARCHAR(255),
+        `status` TINYINT (1) NOT NULL DEFAULT '0',
+        `last_login` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
 
 INSERT INTO
     `mp_users` (
